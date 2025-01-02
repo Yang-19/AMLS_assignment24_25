03b248a0631624dfe4f9041711e0479110bd1b43
Y_M <mengyang011002@outlook.com>
2025-01-02 19:15:02 +0000

    start

--- taskb.py
+++ taskb.py
@@ -6,3 +6,8 @@ from sklearn.datasets import  load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
+import medmnist
+from medmnist import BloodMNIST
+data = np.load('/Users/sammeng/Downloads/Applied_machine_learning/bloodmnist_224.npz')
+# Print all keys in the .npz file
+print("Keys in the .npz file:", data.keys())
